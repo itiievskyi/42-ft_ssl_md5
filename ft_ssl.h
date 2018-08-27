3a43da08905c95babe14c5eebc5702a8b4e4d1c5
Igor Tiievskyi <itiyevskiy@gmail.com>
2018-08-27 16:33:35 +0300

    v.0.03 .h updated

--- ft_ssl.h
+++ ft_ssl.h
@@ -18,15 +18,25 @@
 # define FUNCTIONS 2
 # define FUNC_NAME 7
 
-typedef struct	s_ssl_func
+typedef struct		s_ssl_func
 {
-	char		func[FUNC_NAME];
-	char		type[3];
-	void		(*handler)(int, char**);
-}				t_ssl_func;
+	char			func[FUNC_NAME];
+	char			type[3];
+	void			(*handler)(int, char**);
+}					t_ssl_func;
 
-void			md5(int argc, char **argv);
-void			sha256(int argc, char **argv);
-void			wrong_argument(t_ssl_func handler[], char **argv, int i);
-void			usage(void);
+typedef struct		s_file
+{
+	char			*file;
+	int				p;
+	int				q;
+	int				r;
+	int				s;
+	struct s_file	*next;
+}					t_file;
+
+void				md5(int argc, char **argv);
+void				sha256(int argc, char **argv);
+void				wrong_argument(t_ssl_func handler[], char **argv, int i);
+void				usage(void);
 #endif
