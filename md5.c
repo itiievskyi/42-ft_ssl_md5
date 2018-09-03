247f478dd4ceb1acb13ef8a83f0dbac5fff40308
Igor Tiievskyi <itiyevskiy@gmail.com>
2018-09-03 11:36:59 +0300

    v.0.07: creating string 448mod512

--- md5.c
+++ md5.c
@@ -15,13 +15,40 @@
 void		md5_encrypt(char *str, t_flags *flags)
 {
 	size_t			len;
+	size_t			str_len;
+	size_t			i;
+	int				j;
 	unsigned char	*input;
 
 	if (!str || !flags)
 		return ;
-	len = 512 - (ft_strlen(str) * 8) / 448 + ft_strlen(str) * 8;
+	i = -1;
+	j = -1;
+	str_len = ft_strlen(str);
+	len = 512 - (str_len * 8) % 448 + str_len * 8;
 	input = (unsigned char*)malloc(sizeof(unsigned char) * len / 8 + 1);
-	input[len / 8] = '\0';
+	while (++i < str_len)
+		input[i] = str[i];
+	input[i] = 128;
+	while (++i < len / 8 - 8)
+		input[i] = 0;
+	while (++i - 1 < len / 8 && (++j > -1))
+		input[len / 8 - j - 1] = (str_len * 8 >> (j * 8)) % 256;
 	ft_printf("%s\n", str);
-//	(!flags->s) ? free(str) : 0;
+
+	//	(!flags->s) ? free(str) : 0;
+
+	/* CHECKING */
+
+
+	int y;
+	size_t z = -1;
+	while (++z < len / 8) {
+		for (y = 0; y < 8; y++) {
+			ft_printf("%d", !!((input[z] << y) & 0x80));
+		}
+		ft_printf(" ");
+	}
+	ft_printf("\nstr_len = %d bytes, length = %d bits, or %d bytes\n", str_len, len, len / 8);
+
 }
